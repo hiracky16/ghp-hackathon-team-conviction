781ed77b1043763c44098216f737f53418f8fe0f
hiracky16 <h.piiice16@gmail.com>
2020-02-23 16:50:57 +0900

    app.js 更新

--- app.js
+++ app.js
@@ -78,7 +78,7 @@ export default class App extends Component {
   _animate() {
     const {
       loopLength = 1200, // unit corresponds to the timestamp in source data
-      animationSpeed = 150 // unit time per second
+      animationSpeed = 100 // unit time per second
     } = this.props;
     const timestamp = Date.now() / 1000;
     const loopTime = loopLength / animationSpeed;
